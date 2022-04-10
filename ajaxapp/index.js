a8612622ca229f43e8b47ae60b7bac63d8026775
Yasunori Takahashi <hibriiiiidge@gmail.com>
2022-04-10 11:09:24 +0900

    Add ajaxapp/display

--- ajaxapp/index.js
+++ ajaxapp/index.js
@@ -7,10 +7,41 @@ function fetchUserInfo(userId) {
               console.error("エラーレスポンス", response);
           } else {
               return response.json().then(userInfo => {
-                  console.log(userInfo);
+                const view = escapeHTML`
+                <h4>${userInfo.name} (@${userInfo.login})<h4>
+                <img src="${userInfo.avatar_url} alt="${userInfo.login}" height="100px">
+                <dl>
+                  <dt>Location</dt>
+                  <dt>${userInfo.location}</dt>
+                  <dt>Repositories</dt>
+                  <dt>${userInfo.public_repos}</dt>
+                </dl>
+                `;
+                const result = document.getElementById("result");
+                result.innerHTML = view;
               });
           }
       }).catch(error => {
           console.error(error);
       });
+}
+
+function escapeSpecialChars(str) {
+  return str
+    .replace(/&/g, "&amp;")
+    .replace(/</g, "&lt;")
+    .replace(/>/g, "&gt;")
+    .replace(/"/g, "&quot;")
+    .replace(/'/g, "&#039;")
+}
+
+function escapeHTML(strings, ...values) {
+  return strings.reduce((result, str, i) => {
+    const value = values[i - 1];
+    if (typeof value === "string") {
+      return result + escapeSpecialChars(value) + str;
+    } else {
+      return result + String(value) + str;
+    }
+  });
 }
